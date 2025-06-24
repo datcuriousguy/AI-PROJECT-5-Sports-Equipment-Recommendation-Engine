5e9014f581b6ef7965cf119753a7b70e1d6cb451
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-24 23:42:32 +0530

    Update database_control.py
    importing necessary libraries

--- database_control.py
+++ database_control.py
@@ -3,3 +3,14 @@
 import mysql.connector
 # we need json to convert python lists (refer to tags, say, in readme) into strings for uploading to mysql db - i.e., compatibility
 import json
+
+def create_and_populate_clients_table():
+    # Connect to MySQL...
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="ai_project_5_database"
+    )
+    # create cursor object
+    cursor = conn.cursor()

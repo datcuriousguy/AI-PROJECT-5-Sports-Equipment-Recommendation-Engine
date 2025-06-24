a18f481b9a6ee7af3455b4a844179e545b3d3e58
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-06-24 23:42:13 +0530

    Update database_control.py
    json lib improt

--- database_control.py
+++ database_control.py
@@ -1,3 +1,5 @@
 
 # we need it for manipulating a local mysql db through python
 import mysql.connector
+# we need json to convert python lists (refer to tags, say, in readme) into strings for uploading to mysql db - i.e., compatibility
+import json

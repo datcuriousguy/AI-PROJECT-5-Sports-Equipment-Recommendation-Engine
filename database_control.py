5e10b1ae80b675b537cb0d40769b8fe47f80080b
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-07-10 16:58:33 +0530

    Update database_control.py
    inserting the values like usual

--- database_control.py
+++ database_control.py
@@ -289,3 +289,5 @@ def populate_customers_table():
         email = fake.unique.email()
         preferences = json.dumps(random.choice(preferences_pool))
         device = random.choice(device_types)
+
+        cursor.execute(insert_query, (name, age, email, preferences, device))

4eac58ede149b74caae9d25144186a8be84cacb1
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-07-13 23:21:18 +0530

    Update model.py
    class using nn module with the beginning values to be embedded and used to train the modle

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-
 import torch.nn as nn
 
 """
@@ -8,3 +7,7 @@ Pytorch tracks which parameters need to be optimized and does so accordingly.
 using feedforward function would require rebuilding the layer every time a
 prediction is made, which can be avoided.
 """
+
+class RecommenderNN(nn.Module):  # we start with embed_dim as 32
+    def __init__(self, num_users, num_products, num_interactions, embed_dim=32):
+        super(RecommenderNN, self).__init__()

442153e506ec847ec3b13c1fe239289a596ddc09
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-07-13 23:20:48 +0530

    Update model.py
    importing torch.nn, planning the class

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 
+import torch.nn as nn
+
+"""
+Creating a class called RecommenderNN athat uses the torch nn module.
+instead of a feed forward function, we use __init__ because of its reusability.
+Pytorch tracks which parameters need to be optimized and does so accordingly.
+using feedforward function would require rebuilding the layer every time a
+prediction is made, which can be avoided.
+"""

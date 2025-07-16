54f047a4b725521689a135546cc1a8b269a910f6
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-07-16 23:23:11 +0530

    Update model.py
    planning the query

--- model.py
+++ model.py
@@ -64,3 +64,4 @@ def load_training_data():
 # Understanding the query:
     # We extract all user browsing information using a subquery that aliases browsing_history table as bh,
     # we use the join query to create a one-to-one mapping of browsing history with customer ids cause
+    # there is no use in having browsing history if it can't be linked to customer ids.
